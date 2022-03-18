955dbe99ac8d225de8555de0fdec29f5e65eaa8e
caojiali1996 <562393892@qq.com>
2022-03-18 10:54:36 +0800

    added auth

--- src/App.js
+++ src/App.js
@@ -6,25 +6,18 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AmplifySignOut />
-        <header className="App-header">
+        
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        </header> */}
+        
       </div>
     );
   }
